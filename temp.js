886212248bd051034b8a9dc1d10f1779cc0aa287
Kerden <1209188769@qq.com>
2022-11-23 11:21:47 +0800

    add license

---
+++ temp.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         hello world
+// @namespace    http://kpx.com
+// @version      0.1
+// @description  new script
+// @author       kpx
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @grant        none
+// ==/UserScript==
+
+(
+    function() {
+    'use strict';
+    console.log('hello world');
+    // Your code here...
+}
+)();
